de470b6148de62021a730bc08a887e32a1b0242f
Lauren Kwon <kwonlauren@snu.ac.kr>
2024-11-16 23:05:25 +0900

    feat: thorn and etc

---
+++ UnityProject/GravitableEscape/Assets/Script/UIManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI lifeText;
+    PlayerManager playerManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeText.text = $"Life: {playerManager.life}";
+    }
+}
